6b3415af98dc343638386914cb07727e13f56cdf
Animesh Gupta <97246133+aniimessh@users.noreply.github.com>
2023-01-28 12:59:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'lightRed':'#E42C56',
+        'darkBlue': '#323D4E',
+      }
+    },
+  },
+  plugins: [],
+}
